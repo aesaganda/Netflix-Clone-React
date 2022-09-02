35aea588f5d43e6cde4ab85e47ce7fa50495d2d2
Melih Afsar <melih_afsar@hotmail.com.tr>
2022-09-02 16:06:30 +0300

    test img route

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,13 +3,10 @@ import FormLogin from '../components/FormLogin'
 import Icon from "/static/img/body/netflixLogo.png";
 
 function LoginPage() {
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
   return (
     <>
       <div className='form-section'>
-        <a className="logo" href="https://www.netflix.com/" target="_blank"><img src={myIcon.src} /></a>
+        <a className="logo" href="https://www.netflix.com/" target="_blank"><img src={Icon} /></a>
         <FormLogin />
       </div>
     </>
